b07dba960ce1204ca9e01ed0601b9a0b2e51352d
unbell501 <unsol4108@naver.com>
2022-09-20 15:49:28 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('hello, world!')
+hello, world!
